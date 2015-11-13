91cc9deca9af865182a909e824aa4031340af988
michel <michel.szinavel@gmail.com>
2015-11-13 03:45:16 -0300

    finished the principal functionality of the program, make it work, SIMPLEX RULZ!

--- app/controllers/simplex_controller.rb
+++ app/controllers/simplex_controller.rb
@@ -4,11 +4,50 @@ class SimplexController < ApplicationController
 		@variables = params[:variables].to_i
 		@restrictions = params[:restrictions].to_i
 
+		#raise @variables.inspect
+	end
+
+	def solution
+		@to_solve = []
+
+		@variables = params[:variables].to_i
+		@restrictions = params[:restrictions].to_i
+
+		@objectives = []
+
+		(1..@variables).each do |x|
+			@objectives.push params["objectives_"+"#{x}"].to_i
+		end
+
+		@values = []
+
+		(1..@restrictions).each do |x|
+			(1..@variables).each do |i|
+				@values.push params["values_"+"#{x}_#{i}"].to_i
+			end
+		end
+
+		@values = @values.each_slice(@variables).to_a
+
+		@results = []
+
+		(1..@restrictions).each do |x|
+			@results.push params["results_"+"#{x}"].to_i
+		end
+
+		@to_solve.push @objectives
+		@to_solve.push @values
+		@to_solve.push @results
+
+		#@simplex = Simplex.new([1, 1, 1], [[ 2,  1, 1], [ 1,  2, 1], [ 2,  1, 2], [ 7,  2, 4]],[4, 3, 7, 5])
+		@simplex = Simplex.new(@objectives, @values, @results)
+
+		#raise @simplex.solution.inspect
 	end
 
 	private
 	def simplex_params
-		params.require(:simplex).permit(:objective, :variables, :restrictions)
+		params.require(:simplex).permit(:objectives, :variables, :restrictions, :values, :results)
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
   post '/simplex/index' => 'simplex#index'
+  post '/simplex/solution' => 'simplex#solution'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
